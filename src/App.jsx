135449de31d584532e4a8ef68722a33d6b2b7a0c
vmqxdv <vquintilio@gmail.com>
2025-04-11 18:34:43 +0200

    feat: Eseguito il log degli attori (dopo aver corretto il link -_-)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,31 @@
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function App() {
+  const [Actresses, SetActresses] = useState([]);
+  const [Actors, SetActors] = useState([]);
+
+
+  const FetchActresses = () => {
+    Axios.get('https://www.freetestapi.com/api/v1/actresses')
+    .then((Res) => SetActresses(Res.data))
+  };
+  
+  const FetchActors = () => {
+    Axios.get('https://www.freetestapi.com/api/v1/actors')
+      .then((Res) => SetActors(Res.data))
+  };
+
+
+  
+  useEffect(() => {
+    FetchActresses();
+    FetchActors();
+  }, []);
+
+
+  console.log(Actresses, Actors);
+
   return (
     <>
     </>

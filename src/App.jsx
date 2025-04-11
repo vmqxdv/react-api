aec349e65f37d32dd1c464a909349d2b1452cdd0
vmqxdv <vquintilio@gmail.com>
2025-04-11 18:54:24 +0200

    feat: Mostrate in pagina dati attori/attrici

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ export default function App() {
   const [Actors, SetActors] = useState([]);
 
 
+
   const FetchActresses = () => {
     Axios.get('https://www.freetestapi.com/api/v1/actresses')
     .then((Res) => SetActresses(Res.data))
@@ -24,10 +25,40 @@ export default function App() {
   }, []);
 
 
-  console.log(Actresses, Actors);
+
+  const GenerateCards = (data) => {
+    return data.map((element, i) => {
+      const { image, name, nationality, birth_year: birthYear, death_year: deathYear, biography, awards } = element;
+
+      return (
+        <div key={name + `-${i}`} className='card'>
+          <div className='img-container'>
+            <img src={image} alt={name} />
+          </div>
+          <div className='info'>
+            <h2 className='name'>{name}</h2>
+            <div className='nationality'>{nationality}</div>
+            <div className='birth'>{birthYear} - {deathYear ?? 'presente'}</div>
+            <p className='biography'>{biography}</p>
+            <hr />
+            <p className='awards'>Riconoscimenti: {awards}</p>
+          </div>
+        </div>
+      )
+    })
+  };
+
+
 
   return (
-    <>
-    </>
+    <div className='cards'>
+      <div key='actresses' className='actresses'>
+        {GenerateCards(Actresses)}
+      </div>
+
+      <div key='actors' className='actors'>
+        {GenerateCards(Actors)}
+      </div>
+    </div>
   )
 };
